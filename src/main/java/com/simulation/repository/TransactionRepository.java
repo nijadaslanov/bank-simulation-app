87cd09092edf22d29cb9468589bd1a8787e146ed
Aslanov Nijad <aslanlinicad@gmail.com>
2022-11-22 02:29:56 -0800

    makeTransfer

--- src/main/java/com/simulation/repository/TransactionRepository.java
+++ src/main/java/com/simulation/repository/TransactionRepository.java
@@ -7,11 +7,16 @@ import java.util.ArrayList;
 import java.util.List;
 @Component
 public class TransactionRepository {
+
     public static List<Transaction> transactionList = new ArrayList<>();
 
     public Transaction save(Transaction transaction) {
         transactionList.add(transaction);
         return transaction;
 
+    }
+
+    public List<Transaction> findAll(){
+        return transactionList;
     }
 }
--- src/main/java/com/simulation/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/simulation/service/impl/TransactionServiceImpl.java
@@ -119,8 +119,7 @@ public class TransactionServiceImpl implements TransactionService {
     @Override
     public List<Transaction> findAllTransaction() {
 
-
-        return null;
+        return transactionRepository.findAll();
 
     }
 }
