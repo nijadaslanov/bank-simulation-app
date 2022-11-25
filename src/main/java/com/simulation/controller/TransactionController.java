832030f33eaecb06904bff43ec56e21c62b0b9b7
Aslanov Nijad <aslanlinicad@gmail.com>
2022-11-24 19:10:54 -0800

    make-transfer

---
+++ src/main/java/com/simulation/controller/TransactionController.java
@@ -0,0 +1,27 @@
+package com.simulation.controller;
+
+
+import com.simulation.service.AccountService;
+import com.simulation.service.TransactionService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@AllArgsConstructor
+@Controller
+public class TransactionController {
+
+    private final AccountService accountService;
+    private final TransactionService transactionService;
+
+    @GetMapping("/make-transfer")
+    public String makeTransfer(Model model) {
+
+
+
+        return "transaction/make-transfer";
+    }
+
+
+}
